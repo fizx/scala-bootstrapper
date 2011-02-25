fa533fc910a26ddcf333957faf4904794f25e1fc
Kyle Maxwell <kmaxwell@twitter.com>
2011-02-25 13:34:45 -0800

    more doc

--- lib/template/src/test/scala/com/twitter/birdname/BirdNameServiceSpec.scala
+++ lib/template/src/test/scala/com/twitter/birdname/BirdNameServiceSpec.scala
@@ -4,13 +4,18 @@ import org.specs.Specification
 import com.twitter.admin._
 
 class BirdNameServiceSpec extends Specification {
+  // If you do multiple integration tests, you might want to factor this 
+  // into a superclass.
   val env = RuntimeEnvironment(this, Array("-f", "config/test.scala"))
   val impl = env.loadRuntimeConfig[BirdNameService]
-  ServiceTracker.shutdown
+  
+  // You don't really want the thrift server active, particularly if you
+  // are running repetitively via ~test
+  ServiceTracker.shutdown // all services
 
   "BirdNameService" should {
 
-    // TODO: Please implement
+    // TODO: Please implement your own tests
 
     "hello world" in {
       impl.hello.get() mustEqual "world"

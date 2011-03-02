0635ef898dc5ff95a367214fd36d5ff961052e35
Robey Pointer <robey@twitter.com>
2011-03-02 14:31:59 -0800

    fix bugs.

--- lib/template/src/test/scala/com/twitter/birdname/BirdNameServiceSpec.scala
+++ lib/template/src/test/scala/com/twitter/birdname/BirdNameServiceSpec.scala
@@ -6,14 +6,11 @@ import com.twitter.admin._
 class BirdNameServiceSpec extends Specification {
   val env = RuntimeEnvironment(this, Array("-f", "config/test.scala"))
   val impl = env.loadRuntimeConfig[BirdNameService]
-  ServiceTracker.shutdown
+  ServiceTracker.shutdown()
 
   "BirdNameService" should {
-
-    // TODO: Please implement
-
-    "hello world" in {
-      impl.hello.get() mustEqual "world"
+    "create a name" in {
+      impl.getName().get() mustEqual "Jabberwocky"
     }
   }
 }
